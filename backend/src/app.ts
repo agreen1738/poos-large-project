0606abb76b9a4c873cc2a5b845f5059f9989d232
Gradi Mbuyi <gradimbuyi@outlook.com>
2025-10-18 13:09:05 -0400

    clean up app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,24 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import auth_routes from './routes/auth.ts';
 import type { Response, Request, NextFunction } from 'express';
 
 const app = express();
 
-// Middlewares
-app.use(cors);
+// Middleware
+app.use(cors());
 app.use(express.json());
 
 // CORS Headers
 app.use((req: Request, res: Response, next: NextFunction) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PATCH, DELETE, OPTIONS'
-    );
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
     next();
 });
 
@@ -26,5 +21,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 app.get('/', (req: Request, res: Response) => {
     res.send('Backend is running!');
 });
+
+app.use('/api/auth', auth_routes);
 
 export default app;
